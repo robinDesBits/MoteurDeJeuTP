dfda683b0caae6de79d0892d10f4ced835e39bc1
robindesbits <jolan.konig@gmail.com>
2015-10-21 11:20:14 +0200

    base du tp création du dépot

---
+++ GameWindows.h
@@ -0,0 +1,57 @@
+#ifndef GAMEWINDOWS_H
+#define GAMEWINDOWS_H
+#include "openglwindow.h"
+#include <QtGui/QWindow>
+#include <QtGui/QOpenGLFunctions>
+#include <QtGui/QGuiApplication>
+#include <QtGui/QMatrix4x4>
+#include <QtGui/QOpenGLShaderProgram>
+#include <QtGui/QScreen>
+#include <QKeyEvent>
+#include <QtCore/qmath.h>
+
+struct Point
+{
+    float x, y ,z;
+};
+struct Couleur
+{
+    float r, g ,b;
+};
+class GameWindow : public OpenGLWindow
+{
+public:
+    GameWindow();
+
+    void initialize();
+    void render();
+    void keyPressEvent(QKeyEvent *event);
+    void ChargerMap(QString localPath);
+
+private:
+
+    GLuint loadShader(GLenum type, const char *source);
+
+    GLuint m_posAttr;
+    GLuint m_colAttr;
+    GLuint m_matrixUniform;
+    QImage m_image;
+
+    QOpenGLShaderProgram *m_program;
+    int m_frame;
+
+    Point *points;
+    Couleur *couleurs;
+    Point *vertices;
+
+    int rotX=-45;
+    int rotY= 0;
+    int rotZ= 0;
+    int largeur;
+    int hauteur;
+    float deplacerX=1.3;
+    float avancerZ=-3;
+    float scale=0;
+};
+
+#endif // GAMEWINDOWS_H
